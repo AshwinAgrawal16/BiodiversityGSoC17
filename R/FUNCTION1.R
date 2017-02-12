1cceb56c6762c2fc6be4e0be5a3488052e0c6c57
Ashwin Agrawal <ashwin.agrawal.met14@itbhu.ac.in>
2017-02-12 19:42:41 +0530

    Corrected the function for proper pushing of data

--- R/FUNCTION1.R
+++ R/FUNCTION1.R
@@ -16,14 +16,41 @@ FUNCTION<-function(){
       country = "AU",     # Country code for australia
       classKey= 359,      # Class code for mammalia
       from = 'gbif',
-      limit=500,
+      limit=1000,
       minimal=FALSE
     )
   }
 
   if(requireNamespace("rgbif", quietly = TRUE)){
     d1 <- d1$data
-    str(d1)
+
+
+  }
+
+
+  j<-which(is.na(d1[,"decimalLatitude"]))    #Checking decimalLatitude for NA values
+  k<-which(is.na(d1[,"decimalLongitude"]))   #Checking decimalLongitude for NA values
+  i<-which(is.na(d1[,"name"]))               #Checking name for NA values
+  l<-which(is.na(d1[,"countryCode"]))        #Checking countrycode for NA values
+  r<-which(is.na(d1[,"scientificName"]))
+  if(length(i)!=0)
+    d1<-d1[-i,]
+  if(length(k)!=0)
+    d1<-d1[-k,]
+  if(length(j)!=0)
+    d1<-d1[-j,]
+  if(length(l)!=0)
+    d1<-d1[-l,]
+  if(length(r)!=0)
+    d1<-d1[-r,]
+
+
+  #Caveat: while the API accepts scientific names as specified in the DarwinCore Standard, currently some tools only work if the "Genus"+"Specific Epithet" binomial is provided in this field.
+  #Thus, instead of "Puma concolor (Linnaeus, 1771)", we recommend using just "Puma concolor" in the 'scientificName' field.
+  #To follow the abve code copying the name to scientificName column
+  for(i in 1:nrow(d1))
+  {
+    d1[i,"scientificName"]=d1[i,"name"]
   }
 
   #Checking the values to conform to the DarwinCore Standard
@@ -33,14 +60,14 @@ FUNCTION<-function(){
   #scientificName
 
   if(requireNamespace("rgbif", quietly = TRUE)){
-    "decimalLatitude" %in% names(d1)
-    "decimalLongitude" %in% names(d1)
-    "countryCode" %in% names(d1)
-    "scientificName" %in% names(d1)
+    "decimalLatitude" %in% names(D1)
+    "decimalLongitude" %in% names(D1)
+    "countryCode" %in% names(D1)
+    "scientificName" %in% names(D1)
   }
 
   #Pushing the data to Geospatial Quality API.
-
+  #Hard limit of 1000 is set in the function add_flags.
   library(rgeospatialquality)
   if(requireNamespace("rgbif", quietly = TRUE)){
     dd1 <- add_flags(d1)
