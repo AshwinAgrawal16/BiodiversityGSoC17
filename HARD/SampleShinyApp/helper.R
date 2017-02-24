f611ea570606661b1a25d8ab6c45f61a8c870206
Ashwin Agrawal <ashwin.agrawal.met14@itbhu.ac.in>
2017-02-24 18:33:56 +0530

    Create helper.R

---
+++ HARD/SampleShinyApp/helper.R
@@ -0,0 +1,11 @@
+#Hepler function for displaying the map using shiny app
+library(rgbif)
+library(mapr)
+map1 <- function(country,classKey){
+  data <- occ_search(
+    country = country,
+    classKey = as.numeric(classKey),
+    limit= 100,hasCoordinate = T)
+  
+  map_ggmap(data)
+}
